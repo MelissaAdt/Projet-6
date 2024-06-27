34a3463f211f98ba527774a636f40349c3313594
Melissa Andremont <melissa-andremont@live.fr>
2024-06-27 19:46:32 +0200

    ajout de auth.js

--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -1,6 +1,5 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-
 const User = require('../models/User');
 
 
@@ -11,7 +10,8 @@ bcrypt.hash(req.body.password, 10)
         email:req.body.email,
         password: hash
     });
-    user.save()
+    user
+    .save()
     .then(() => res.status(201).json({ message: 'Utilisateur créé !'}))
     .catch(error => res.status(400).json({ error }));
 })

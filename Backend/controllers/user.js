4272303238f3f9f42b97f0445bd7bdb553ecbdec
Melissa Andremont <melissa-andremont@live.fr>
2024-06-27 13:24:43 +0200

    implémentation de la fonction login

--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -18,5 +18,22 @@ bcrypt.hash(req.body.password, 10)
 };
 
 exports.login =(req, res, next) => { 
-
+    User.findOne({email: req.body.email})
+    .then(user => {
+        if (!user) {
+            return res.status(401).json({ message: 'Paire identifiant/mot de passe incorrecte'});
+        }
+        bcrypt.compare(req.body.password, user.password)
+            .then(valid => {
+                if (!valid) {
+                    return res.status(401).json({ message: 'Paire identifiant/mot de passe incorrecte' });
+                }
+                res.status(200).json({
+                    userId: user._id,
+                    token: 'TOKEN'
+                });
+            })
+            .catch(error => res.status(500).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
 };
